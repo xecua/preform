faa93dd5f88f23dbb5f6a3f86a2b9d8e4f3bfc98
xecua <courser4848@gmail.com>
2021-12-17 13:28:30 +0900

    implement Formatter

--- src/main/kotlin/dev/koffein/preform/Format.kt
+++ src/main/kotlin/dev/koffein/preform/Format.kt
@@ -1,14 +1,43 @@
 package dev.koffein.preform
 
+import jp.ac.titech.c.se.stein.core.Context
 import jp.ac.titech.c.se.stein.core.RepositoryRewriter
 import mu.KotlinLogging
+import org.eclipse.jdt.core.ToolFactory
+import org.eclipse.jdt.core.formatter.CodeFormatter
+import org.eclipse.jface.text.Document
+import org.eclipse.jgit.lib.ObjectId
+import java.nio.charset.StandardCharsets
 
 class Format : RepositoryRewriter() {
-    init {
-        logger.debug { "foo" }
-        print("bar")
+    override fun rewriteBlob(blobId: ObjectId, c: Context): ObjectId {
+        if (!c.entry.name.lowercase().endsWith(".java")) {
+            return super.rewriteBlob(blobId, c)
+        }
+
+        val content = String(source.readBlob(c.entry.id, c), StandardCharsets.UTF_8)
+        val formatter = ToolFactory.createCodeFormatter(null)
+
+        val formattingOperation = formatter.format(
+            CodeFormatter.K_COMPILATION_UNIT or CodeFormatter.F_INCLUDE_COMMENTS,
+            content, 0, content.length, 0, "\n"
+        )
+       
+        if (formattingOperation == null) {
+            logger.warn("${blobId.name} was not able to format.")
+            return blobId
+        }
+
+        val doc = Document(content)
+        formattingOperation.apply(doc)
+
+        val newId = target.writeBlob(doc.get().toByteArray(), c)
+
+        logger.debug { "Rewrite blob: ${blobId.name} -> ${newId.name} $c" }
+
+        return newId
     }
-    
+
     companion object {
         private val logger = KotlinLogging.logger {}
     }
