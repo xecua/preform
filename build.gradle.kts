980f5cb015644d48b1b28ee6fc3e85ea16078325
xecua <courser4848@gmail.com>
2022-08-05 18:27:37 +0900

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,17 +15,17 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val kotestVersion = "5.3.0"
+val kotestVersion = "5.4.1"
 
 dependencies {
-    implementation("io.github.microutils:kotlin-logging-jvm:2.1.16")
-    implementation("ch.qos.logback:logback-classic:1.2.8")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.23")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
 
     implementation("com.github.sh5i:git-stein:v0.5.0")
 
-    implementation("info.picocli:picocli:4.6.2")
-    implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.28.0")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:6.0.0.202111291000-r")
+    implementation("info.picocli:picocli:4.6.3")
+    implementation("org.eclipse.jdt:org.eclipse.jdt.core:3.29.0")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.2.0.202206071550-r")
 
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
