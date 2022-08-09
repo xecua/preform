182a564f3bf8f57b60a0674c06ec3e5e4d6a505d
xecua <courser4848@gmail.com>
2022-08-09 17:42:59 +0900

    Reimplement CommentRemover and add test

--- src/main/kotlin/page/caffeine/preform/filters/RemoveComment.kt
+++ src/main/kotlin/page/caffeine/preform/filters/RemoveComment.kt
@@ -17,11 +17,85 @@ class RemoveComment : RepositoryRewriter() {
         return target.writeBlob(afterContent.toByteArray(), c)
     }
 
-    // internal fun rewriteContent(content: String): String =
-    //     CommentRemoverJC(CRConfig.initialize(arrayOf("--linecomment", "remove", "--blockcomment", "remove"))).perform(content)
     internal fun rewriteContent(content: String): String {
-        // CommentRemoverだとコメントを削除することによって発生した(もとから空行ではなかった)空行が生えてしまう ← どうにかする必要がある
-        
-        return content
+        val remover = JavaCommentRemover()
+        return remover.removeComment(content, false)
     }
+}
+
+abstract class CommentRemover {
+    protected abstract fun getLineCommentStart(): List<String>
+    protected abstract fun getBlockCommentStartAndEnd(): List<Pair<String, String>>
+
+    /**
+     * @param keepLine: Whether keep lines if the line become contain whitespaces only after removing the comment.
+     *                  Note: This does not remove lines only with whitespaces from the beginning.
+     */
+    fun removeComment(content: String, keepLine: Boolean): String {
+        var inLineComment = false
+        var inBlockComment = false
+        var existCommentInCurrentLine = false
+        val blockCommentBegins = getBlockCommentStartAndEnd().map { it.first }
+        val blockCommentEnds = getBlockCommentStartAndEnd().map { it.second }
+
+        val currentLine = StringBuilder()
+        val res = StringBuilder()
+
+        // 1行に複数回ブロックコメントが現れることがあることに注意
+
+        var i = 0
+        while (i < content.length) {
+            val c = content[i]
+            if (!inLineComment && blockCommentBegins.any { content.substring(i).startsWith(it) }) {
+                // 行コメントの途中なら判定外
+                inBlockComment = true
+            }
+            if (!inBlockComment && getLineCommentStart().any { content.substring(i).startsWith(it) }) {
+                // ブロックコメントの途中なら判定外
+                inLineComment = true
+            }
+
+            if (c == '\n') {
+                // 改行: \rは必要ならcurrentLineに入っているので気にしなくていい
+                if (!(!keepLine && existCommentInCurrentLine && currentLine.matches(Regex("""^\s*$""")))) {
+                    // (無視した文字があったことによって空白だけになった、かつkeepLineがfalse (マッチは重そうなので最後)) の否定(こっちしか処理がないので)
+                    currentLine.append(c)
+                    res.append(currentLine)
+                }
+
+                currentLine.clear()
+                inLineComment = false
+                existCommentInCurrentLine = false
+            } else {
+                if (!inLineComment && !inBlockComment) {
+                    currentLine.append(c)
+                } else {
+                    existCommentInCurrentLine = true
+                }
+            }
+
+            for (bce in blockCommentEnds) {
+                if (content.substring(i).startsWith(bce)) {
+                    inBlockComment = false
+                    i += bce.length - 1 // 下で+1するのでこっちで引いておく
+                    break
+                }
+            }
+
+            i += 1
+        }
+
+        // EOF: EOLと同じ処理が必要
+        if (!(!keepLine && existCommentInCurrentLine && currentLine.matches(Regex("""^\s*$""")))) {
+            // (無視した文字があったことによって空白だけになった、かつkeepLineがfalse (マッチは重そうなので最後)) の否定(こっちしか処理がないので)
+            res.append(currentLine)
+        }
+
+        return res.toString()
+    }
+}
+
+class JavaCommentRemover : CommentRemover() {
+    override fun getLineCommentStart(): List<String> = listOf("//")
+    override fun getBlockCommentStartAndEnd(): List<Pair<String, String>> = listOf(Pair("/*", "*/"))
 }
---
+++ src/test/kotlin/page/caffeine/preform/filters/RemoveCommentTests.kt
@@ -0,0 +1,47 @@
+package page.caffeine.preform.filters
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class RemoveCommentTests : FunSpec({
+    val it = RemoveComment()
+
+    test("check basic functionality") {
+        it.rewriteContent(
+            """
+            // a
+            // b
+            int i = 0;
+            /* multiple
+             * lines
+             * comment */
+            /* a */ int j = 1; /* b */
+            String foo = "foo"; // after comment
+            String bar = "bar";
+            """.trimIndent()
+            // comment in last line
+        ).shouldBe(
+            """
+            int i = 0;
+             int j = 1; 
+            String foo = "foo"; 
+            String bar = "bar";
+            """.trimIndent()
+        )
+        // 注: コメントによって消滅した行を消すことはできるが、
+        // コメントの前後の空白を取り除くことはできない
+        // また、最後にコメントがあった場合、その前の行の改行文字は残る
+        it.rewriteContent(
+            """
+            String foo = "foo"; // after comment
+            // comment in last line
+            """.trimIndent()
+            // comment in last line
+        ).shouldBe(
+            """
+            String foo = "foo"; 
+            
+            """.trimIndent()
+        )
+    }
+})
