35931e5e62bcd4e99ea83126309865e6dad6baf7
xecua <contact@caffeine.page>
2022-11-24 22:59:02 +0900

    Implement notes inheritance

--- src/main/kotlin/page/caffeine/preform/util/RepositoryRewriter.kt
+++ src/main/kotlin/page/caffeine/preform/util/RepositoryRewriter.kt
@@ -1,8 +1,6 @@
 package page.caffeine.preform.util
 
 import jp.ac.titech.c.se.stein.core.Context
-import jp.ac.titech.c.se.stein.core.EntrySet
-import org.eclipse.jgit.lib.FileMode
 import org.eclipse.jgit.lib.ObjectId
 import org.eclipse.jgit.lib.Ref
 import org.eclipse.jgit.lib.Repository
@@ -17,6 +15,37 @@ open class RepositoryRewriter : jp.ac.titech.c.se.stein.core.RepositoryRewriter(
         this.sourceRepo = sourceRepo
         this.targetRepo = targetRepo
         super.initialize(sourceRepo, targetRepo)
+    }
+
+    override fun rewrite(c: Context?) {
+        setUp(c)
+        if (cacheProvider != null) {
+            cacheProvider.inTransaction {
+                rewriteCommits(c)
+                updateRefs(c)
+                return@inTransaction null
+            }
+        } else {
+            rewriteCommits(c)
+            updateRefs(c)
+        }
+
+        source.writeNotes(c)
+        // transit previous notes
+        val sourceNote = source.readNote(c)
+        target.eachNote({ targetId, sourceIdString ->
+            val originalIdString = sourceNote.getNote(
+                ObjectId.fromString(String(sourceIdString))
+            )?.let { sourceNote ->
+                String(source.readBlob(sourceNote.data, c))
+            }
+            if (originalIdString != null) {
+                target.addNote(targetId, originalIdString, c)
+            }
+        }, c)
+        target.writeNotes(c)
+
+        cleanUp(c)
     }
 
     override fun confirmStartRef(ref: Ref?, c: Context?): Boolean = true
