ecb146e3c92156f05f8ad0fe6140917df6ab992c
xecua <contact@caffeine.page>
2022-11-27 18:28:11 +0900

    Add option to save intermediate repository

--- src/main/kotlin/page/caffeine/preform/Preform.kt
+++ src/main/kotlin/page/caffeine/preform/Preform.kt
@@ -45,14 +45,14 @@ import java.util.concurrent.Callable
     ]
 )
 class Preform : Callable<Int> {
-    @Parameters(index = "0", paramLabel = "<repo>", description = ["Source repository path"])
+    @Parameters(index = "0", paramLabel = "<source>", description = ["Source repository path"])
     lateinit var source: File
 
     // @Parameters(index = "1", paramLabel = "<filter>", description = ["Using filter"], arity="1..*")
     var filters: MutableList<RepositoryRewriter> = mutableListOf()
     var directories: MutableList<File> = mutableListOf()
 
-    @Parameters(index = "1", paramLabel = "<repo>", description = ["Target repository path"])
+    @Parameters(index = "1", paramLabel = "<destination>", description = ["Destination path"])
     lateinit var target: File
 
     @Option(names = ["-d", "--duplicate"], description = ["Duplicate source repo before rewriting"])
@@ -63,6 +63,13 @@ class Preform : Callable<Int> {
 
     @Option(names = ["--bare"], description = ["treat that repos are bare"])
     var isBare = false
+
+    @Option(
+        names = ["-s", "--save"],
+        description = ["Save intermediate results.", "In this case, <destination> become parent directory of the repositories."]
+    )
+    var saveRepository = false
+    // 中間リポジトリの命名規則を指定できるようにしたい
 
     @Option(
         names = ["--log"],
@@ -90,9 +97,7 @@ class Preform : Callable<Int> {
             setLoggerLevel("org.eclipse.jgit", Level.INFO)
         }
 
-        openRepositories(filters.size) { source, target, i ->
-            val filter = filters[i]
-
+        openRepositories { source, target, filter ->
             filter.initialize(source, target)
             log.info { "Start rewriting by $filter, ${source.directory} -> ${target.directory}" }
             val c = Context.init()
@@ -107,44 +112,49 @@ class Preform : Callable<Int> {
         return 0
     }
 
-    private fun openRepositories(times: Int, action: (Repository, Repository, Int) -> Unit) {
-        if (times <= 0) {
+    private fun openRepositories(action: (Repository, Repository, RepositoryRewriter) -> Unit) {
+        if (filters.isEmpty()) {
             return
         }
 
-        for (i in 0 until times) {
+        if (isCleaning && target.exists()) {
+            log.info { "Cleaning destination repository $target" }
+            target.deleteRecursively()
+        }
+
+        var repoName = source.name
+        filters.forEachIndexed { i, filter ->
             val src = if (i == 0) {
                 source
             } else {
                 directories[i - 1]
             }
 
-            val dst = if (i == times - 1) {
-                if (isCleaning && target.exists()) {
-                    log.info { "Cleaning destination repository $target" }
-                    target.deleteRecursively()
-                }
-                target
+            val dst = if (saveRepository) {
+                repoName += "-${filters::class.simpleName}"
+                target.resolve(repoName)
             } else {
-                val dir = Files.createTempDirectory(i.toString()).toFile()
-                dir.deleteRecursively() // 名前だけ確保して削除
-                directories.add(dir)
-                dir
+                if (i == filters.size - 1) {
+                    target
+                } else {
+                    val dir = Files.createTempDirectory(i.toString()).toFile()
+                    dir.deleteRecursively() // 名前だけ確保して削除
+                    dir
+                }
+            }
+            directories.add(dst)
+
+            if (i == filters.size - 1 && isDuplicating) {
+                log.info { "Duplicating source repository $source to ${directories.last()}" }
+                source.copyTo(directories.last(), true)
             }
 
             createRepository(src, false).use { source ->
                 createRepository(dst, true).use { target ->
-                    action(source, target, i)
+                    action(source, target, filter)
                 }
             }
         }
-
-        // 最後にsrcのファイルをdstに移す? (若干効率が悪いかもしれない)
-        if (isDuplicating) {
-            log.info { "Duplicating source repository $source to $target" }
-            source.copyTo(target, true)
-        }
-
     }
 
     private fun createRepository(dir: File, createIfAbsent: Boolean): Repository {
