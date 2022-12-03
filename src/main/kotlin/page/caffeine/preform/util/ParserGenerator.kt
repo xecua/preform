3611049d88856dd32c4369b5c582ca953796c1a6
xecua <contact@caffeine.page>
2022-12-03 17:06:15 +0900

    stop using parser as singleton

--- src/main/kotlin/page/caffeine/preform/util/ParserGenerator.kt
+++ src/main/kotlin/page/caffeine/preform/util/ParserGenerator.kt
@@ -5,24 +5,16 @@ import org.eclipse.jdt.core.dom.AST
 import org.eclipse.jdt.core.dom.ASTParser
 import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants
 
-var parser: ASTParser? = null
-
-fun generateParser(): ASTParser {
-    if (parser == null) {
-        parser = ASTParser.newParser(AST.getJLSLatest()).also {
-            @Suppress("UNCHECKED_CAST")
-            val options =
-                (DefaultCodeFormatterConstants.getEclipseDefaultSettings() as MutableMap<String, String>).also {
-                    it[JavaCore.COMPILER_COMPLIANCE] = JavaCore.VERSION_1_8
-                    it[JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM] = JavaCore.VERSION_1_8
-                    it[JavaCore.COMPILER_SOURCE] = JavaCore.VERSION_1_8
-                    it[JavaCore.COMPILER_DOC_COMMENT_SUPPORT] = JavaCore.ENABLED
-                }
-            it.setCompilerOptions(options)
-
-            it.setEnvironment(null, null, null, true)
+fun generateParser(): ASTParser = ASTParser.newParser(AST.getJLSLatest()).also {
+    @Suppress("UNCHECKED_CAST")
+    val options =
+        (DefaultCodeFormatterConstants.getEclipseDefaultSettings() as MutableMap<String, String>).also {
+            it[JavaCore.COMPILER_COMPLIANCE] = JavaCore.VERSION_1_8
+            it[JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM] = JavaCore.VERSION_1_8
+            it[JavaCore.COMPILER_SOURCE] = JavaCore.VERSION_1_8
+            it[JavaCore.COMPILER_DOC_COMMENT_SUPPORT] = JavaCore.ENABLED
         }
-    }
+    it.setCompilerOptions(options)
 
-    return parser as ASTParser
+    it.setEnvironment(null, null, null, true)
 }
