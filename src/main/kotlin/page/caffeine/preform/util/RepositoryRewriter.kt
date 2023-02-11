e975b41de6fa1e172fac1ba3bfc6780b469d9cf0
xecua <contact@caffeine.page>
2023-02-11 20:01:13 +0900

    cleanup RepositoryRewriter.kt

--- src/main/kotlin/page/caffeine/preform/util/RepositoryRewriter.kt
+++ src/main/kotlin/page/caffeine/preform/util/RepositoryRewriter.kt
@@ -2,10 +2,8 @@ package page.caffeine.preform.util
 
 import jp.ac.titech.c.se.stein.core.Context
 import org.eclipse.jgit.lib.ObjectId
-import org.eclipse.jgit.lib.Ref
 import org.eclipse.jgit.lib.Repository
 
-// workaround for link https://github.com/sh5i/git-stein/commit/d43f0971402f8aec2af46d569e0bae14272e6321 
 open class RepositoryRewriter : jp.ac.titech.c.se.stein.core.RepositoryRewriter() {
     // 許せ
     protected var sourceRepo: Repository? = null
